6036a5f6a2036ec69c3d8aa87e959270652d9e6c
AmineFanid <fan.aminee@gmail.com>
2026-06-17 15:44:45 -0400

    modifs de store_manager.py

--- src/store_manager.py
+++ src/store_manager.py
@@ -15,14 +15,22 @@ from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
  
 app = Flask(__name__)
 
-# Ajoutez l'import en haut
-import threading
-
 # Auto-generate Redis reports 2 seconds after startup (to give enough time for the DB to start up as well). Once done for the first time, repeat it recursively every 60s to refresh the cache.
 def generate_reports_and_cache():
-    threading.Timer(2.0, get_report_highest_spending_users, args=(True,)).start()
-    threading.Timer(2.0, get_report_best_selling_products, args=(True,)).start()
-    threading.Timer(60.0, generate_reports_and_cache).start()
+    # Créer les threads
+    t1 = threading.Timer(2.0, get_report_highest_spending_users, args=(True,))
+    t2 = threading.Timer(2.0, get_report_best_selling_products, args=(True,))
+    t3 = threading.Timer(60.0, generate_reports_and_cache)
+    
+    # Les définir comme "Daemons" (ils mourront avec le programme principal)
+    t1.daemon = True
+    t2.daemon = True
+    t3.daemon = True
+    
+    # Lancer les threads
+    t1.start()
+    t2.start()
+    t3.start()
 
 # Start the first execution
 generate_reports_and_cache()
